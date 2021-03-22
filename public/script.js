38b523b6aff7db439e64a39c6b427ce04291f90f
spdimmy <spdimmy@gmail.com>
2021-03-23 00:03:19 +0100

    add header and menu with css target, dragging for menu

--- public/script.js
+++ public/script.js
@@ -43,6 +43,8 @@ document.addEventListener('DOMContentLoaded', function () {
       }
 
       if (step === 3) {
+        triggerElement.removeEventListener('animationend', update);
+
         // Add symbols with timeout
         for (let i = 0; i < symbolsCount; i++) {
           setTimeout(() => {
@@ -58,6 +60,57 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     update();
-    triggerElement.addEventListener('click', click);
+    triggerElement.addEventListener('click', click, false);
+  })();
+
+  (function () {
+    const draggableEl = document.querySelector('.header');
+    const dragTrigger = document.querySelector('.header__drag');
+    const headerHeight = getComputedStyle(document.documentElement).getPropertyValue('--header-height');
+
+    let isDragged = false;
+    let startPosition = null;
+
+    let vibrateDistance = 0;
+
+    function updateTranslate(top) {
+      draggableEl.style.transform = `translateY(${top}px) translateY(-${headerHeight})`;
+    }
+
+    function dragStart(e) {
+      isDragged = true;
+      draggableEl.style.transition = '';
+      startPosition = e.clientY || e.touches[0].clientY;
+    }
+
+    function dragEnd() {
+      if (!isDragged) return;
+      isDragged = false;
+      startPosition = null;
+      draggableEl.style.transform = '';
+      draggableEl.style.transition = 'transform 0.5s ease-out';
+      vibrateDistance = 0;
+
+      draggableEl.addEventListener('transitionend', () => draggableEl.style.transition = '');
+    }
+
+    function dragMove(e) {
+      if (!isDragged) return;
+
+      const pos = e.clientY || e.touches[0].clientY;
+      updateTranslate(pos);
+
+      if (pos > vibrateDistance + 40) {
+        navigator.vibrate(30);
+        vibrateDistance = pos;
+      }
+    }
+
+    dragTrigger.addEventListener('mousedown', dragStart, false);
+    dragTrigger.addEventListener('touchstart', dragStart, false);
+    document.addEventListener('mousemove', dragMove, true);
+    document.addEventListener('touchmove', dragMove, true);
+    document.addEventListener('mouseup', dragEnd, false);
+    document.addEventListener('touchend', dragEnd, false);
   })();
 });
