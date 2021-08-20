cb4bd91d9c555350f49af96e0fbca736aacb1bd8
spdimmy <spdimmy@gmail.com>
2021-08-20 23:29:25 +0200

    add scss, prettier

--- src/components/fun-block/fun-block.jsx
+++ src/components/fun-block/fun-block.jsx
@@ -1,14 +1,9 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 
 const FunBlock = () => {
   useEffect(() => {
-    const triggerElement = document.querySelector('.fun-block__title');
-    const phrases = [
-      `This's my webpage`,
-      `GO!`,
-      `<3`,
-      `Log Out`,
-    ];
+    const triggerElement = document.querySelector(".fun-block__title");
+    const phrases = [`This's my webpage`, `GO!`, `<3`, `Log Out`];
 
     let step = 1;
     let prevStep = null;
@@ -21,10 +16,10 @@ const FunBlock = () => {
 
     function updateClass() {
       // Remove prev class
-      if (prevStep !== null) triggerElement.classList.remove(`fun-block__title--step-${prevStep}`);
+      if (prevStep !== null)
+        triggerElement.classList.remove(`fun-block__title--step-${prevStep}`);
 
       triggerElement.classList.add(`fun-block__title--step-${step}`);
-
     }
 
     function click() {
@@ -43,16 +38,16 @@ const FunBlock = () => {
       // Transit
       if (step === 2) {
         navigator.vibrate(400);
-        triggerElement.addEventListener('animationend', update);
+        triggerElement.addEventListener("animationend", update);
       }
 
       if (step === 3) {
-        triggerElement.removeEventListener('animationend', update);
+        triggerElement.removeEventListener("animationend", update);
 
         // Add symbols with timeout
         for (let i = 0; i < symbolsCount; i++) {
           setTimeout(() => {
-            triggerElement.textContent += ' <3';
+            triggerElement.textContent += " <3";
             navigator.vibrate(20);
             if (i === symbolsCount - 1) update();
           }, 30 * i);
@@ -62,14 +57,16 @@ const FunBlock = () => {
       prevStep = step;
     }
 
-    triggerElement.addEventListener('click', click, false);
+    triggerElement.addEventListener("click", click, false);
   }, []);
 
   return (
     <section id="bounce-harm" className="fun-block">
-      <h1 className="fun-block__title fun-block__title--step-1">This's my webpage</h1>
+      <h1 className="fun-block__title fun-block__title--step-1">
+        This's my webpage
+      </h1>
     </section>
-  )
+  );
 };
 
-export default FunBlock
+export default FunBlock;
